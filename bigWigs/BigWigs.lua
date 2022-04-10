f909ce86e93965d7c4b1dcc2ff8196707475a287
Ivo <i.m.stoilov@gmail.com>
2022-04-10 11:38:20 +0300

    [Backup] First Snapshot of the UI
    Backed up: ElvUI/Grid2/BigWigs/MSBT/Details/WeakAura

---
+++ bigWigs/BigWigs.lua
@@ -0,0 +1,791 @@
+
+BigWigs3DB = {
+	["global"] = {
+		["watchedMovies"] = {
+			[957] = true,
+			[-575] = true,
+			[-593] = {
+				true, -- [1]
+				true, -- [2]
+			},
+			[-609] = true,
+			[958] = true,
+			[956] = true,
+			[-573] = true,
+			[-607] = true,
+		},
+	},
+	["namespaces"] = {
+		["BigWigs_Plugins_Alt Power"] = {
+		},
+		["BigWigs_Plugins_BossBlock"] = {
+		},
+		["BigWigs_Bosses_Lords of Dread"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[360319] = 3129591,
+					[360146] = 3064311,
+					[360012] = 3064311,
+					[360717] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Grand Proctor Beryllia"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[325360] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Vigilant Guardian"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[360412] = 3129591,
+					[361001] = 3064311,
+					[359610] = 3064311,
+					[360403] = 3064039,
+					[364881] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Plugins_Statistics"] = {
+		},
+		["BigWigs_Bosses_Anduin Wrynn"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["custom_off_repeating_blasphemy"] = true,
+					[361989] = 3129591,
+					[366849] = 3064311,
+					[365021] = 3064311,
+					[365120] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Lihuvim, Principal Architect"] = {
+			["profiles"] = {
+				["Healer Raid"] = {
+					[360869] = 3064311,
+					[363088] = 3129591,
+					[362622] = 3064311,
+					["berserk"] = 18,
+					[363795] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Bosses_Mailroom Mayhem"] = {
+			["profiles"] = {
+				["Healer M+"] = {
+					[346742] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Plugins_AltPower"] = {
+		},
+		["BigWigs_Bosses_Halls of Atonement Trash"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[326409] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Plugins_Raid Icons"] = {
+		},
+		["BigWigs_Bosses_The Jailer"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[359809] = 0,
+					[366285] = 3064311,
+					[365436] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[362721] = 3064311,
+					[362885] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Plugins_Messages"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["emphPosition"] = {
+						nil, -- [1]
+						nil, -- [2]
+						5.079336166381836, -- [3]
+						57.9048957824707, -- [4]
+					},
+					["normalPosition"] = {
+						nil, -- [1]
+						nil, -- [2]
+						-1.015280365943909, -- [3]
+						-156.6036834716797, -- [4]
+					},
+				},
+			},
+		},
+		["BigWigs_Plugins_Victory"] = {
+		},
+		["BigWigs_Bosses_Ingra Maloch"] = {
+			["profiles"] = {
+				["Healer Raid"] = {
+					[328756] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Surgeon Stitchflesh"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[322681] = 3129847,
+					[343556] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Plugins_Sounds"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["Long"] = {
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[365021] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							["stages"] = "Awww Crap",
+							[364962] = "BigWigs: [DBM] Destruction (Kil'jaeden)",
+						},
+						["BigWigs_Bosses_Surgeon Stitchflesh"] = {
+							[322681] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+					},
+					["Warning"] = {
+						["BigWigs_Bosses_High Adjudicator Aleez"] = {
+							[323650] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Rygelon"] = {
+							[362806] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Prototype Pantheon"] = {
+							[360687] = "BigWigs: Raid Warning",
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[364881] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Surgeon Stitchflesh"] = {
+							[343556] = "Voice: Run Away",
+						},
+						["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+							[362721] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Lihuvim, Principal Architect"] = {
+							[362622] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+							[365418] = "Air Horn",
+						},
+						["BigWigs_Bosses_The Jailer"] = {
+							[360281] = "BigWigs: [DBM] Flag Taken (PvP)",
+							[366285] = "BigWigs: [DBM] Flag Taken (PvP)",
+							[362028] = "BigWigs: [DBM] Run Away Little Girl (Big Bad Wolf)",
+						},
+					},
+					["Info"] = {
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[366849] = "BigWigs: Info",
+						},
+						["BigWigs_Bosses_Prototype Pantheon"] = {
+							[360687] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Lords of Dread"] = {
+							[360012] = "Boxing Arena Gong",
+						},
+						["BigWigs_Bosses_The Necrotic Wake Trash"] = {
+							[323496] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+					},
+					["Alarm"] = {
+						["BigWigs_Bosses_Kryxis the Voracious"] = {
+							[319713] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_Mistcaller"] = {
+							[321834] = "BigWigs: Alarm",
+						},
+						["BigWigs_Bosses_Halls of Atonement Trash"] = {
+							[325701] = "Voice: Cross",
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[359610] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+						["BigWigs_Bosses_The Necrotic Wake Trash"] = {
+							[338606] = "Voice: Run Away",
+						},
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[366849] = "BigWigs: [DBM] Beware (Algalon)",
+							[361815] = "BigWigs: Alarm",
+						},
+						["BigWigs_Bosses_Lihuvim, Principal Architect"] = {
+							[360869] = "BigWigs: [DBM] Run Away Little Girl (Big Bad Wolf)",
+						},
+						["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+							[362885] = "BigWigs: Alert",
+						},
+						["BigWigs_Bosses_Halondrus the Reclaimer"] = {
+							[365297] = "BigWigs: [DBM] Flag Taken (PvP)",
+						},
+					},
+					["Alert"] = {
+						["BigWigs_Bosses_Rygelon"] = {
+							[363533] = "BigWigs: [DBM] Destruction (Kil'jaeden)",
+						},
+						["BigWigs_Bosses_Lords of Dread"] = {
+							[360229] = "BigWigs: [DBM] Beware (Algalon)",
+							[360146] = "BigWigs: [DBM] Flag Taken (PvP)",
+							[360319] = "BigWigs: [DBM] Destruction (Kil'jaeden)",
+						},
+						["BigWigs_Bosses_Spires of Ascension Trash"] = {
+							[327413] = "Voice: Cross",
+						},
+						["BigWigs_Bosses_Lord Chamberlain"] = {
+							[323150] = "BigWigs: [DBM] Beware (Algalon)",
+						},
+						["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+							[361018] = "BigWigs: [DBM] Flag Taken (PvP)",
+							[361513] = "BigWigs: [DBM] Beware (Algalon)",
+						},
+						["BigWigs_Bosses_The Necrotic Wake Trash"] = {
+							[334748] = "BigWigs: [DBM] Destruction (Kil'jaeden)",
+						},
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[362405] = "Voice: Triangle",
+							[365295] = "Voice: Moon",
+						},
+						["BigWigs_Bosses_Lihuvim, Principal Architect"] = {
+							[363795] = "Boxing Arena Gong",
+						},
+						["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+							[362849] = "BigWigs: [DBM] Destruction (Kil'jaeden)",
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[360412] = "Voice: Moon",
+							[361001] = "BigWigs: Alert",
+						},
+					},
+				},
+			},
+		},
+		["BigWigs_Bosses_Prototype Pantheon"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[361789] = 3129591,
+					[360687] = 3064311,
+				},
+				["Healer Raid"] = {
+					[360687] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Plugins_Wipe"] = {
+		},
+		["BigWigs_Bosses_High Adjudicator Aleez"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[323650] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Bosses_Zo'phex the Sentinel"] = {
+			["profiles"] = {
+				["Healer M+"] = {
+					[345770] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Bosses_Kryxis the Voracious"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[319685] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Plugins_Countdown"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["bossCountdowns"] = {
+						["BigWigs_Bosses_Kryxis the Voracious"] = {
+							[319685] = "English: David",
+						},
+						["BigWigs_Bosses_Lords of Dread"] = {
+							[360319] = "English: David",
+						},
+						["BigWigs_Bosses_Grand Proctor Beryllia"] = {
+							[325360] = "English: David",
+						},
+						["BigWigs_Bosses_Echelon"] = {
+							[319941] = "English: David",
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[360412] = "English: David",
+						},
+						["BigWigs_Bosses_Executor Tarvold"] = {
+							[322554] = "English: David",
+						},
+						["BigWigs_Bosses_Halls of Atonement Trash"] = {
+							[326409] = "English: David",
+						},
+						["BigWigs_Bosses_Lihuvim, Principal Architect"] = {
+							[363088] = "English: David",
+						},
+						["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+							[361966] = "English: David",
+							[363200] = "English: David",
+						},
+						["BigWigs_Bosses_Halondrus the Reclaimer"] = {
+							[360115] = "English: David",
+						},
+					},
+					["position"] = {
+						"CENTER", -- [1]
+						"CENTER", -- [2]
+						nil, -- [3]
+						178.0951995849609, -- [4]
+					},
+					["textEnabled"] = false,
+				},
+			},
+		},
+		["BigWigs_Bosses_Executor Tarvold"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[322554] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Rygelon"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[362806] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Plugins_Colors"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["barColor"] = {
+						["BigWigs_Bosses_Halls of Atonement Trash"] = {
+							[326409] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Plugins_Colors"] = {
+							["default"] = {
+								0.2196078431372549, -- [1]
+								0.3450980392156863, -- [2]
+								0.6431372549019607, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Lords of Dread"] = {
+							[360146] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+							[360319] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_The Jailer"] = {
+							[360562] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+							[363893] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+							[361966] = {
+								0.9921568627450981, -- [1]
+								1, -- [2]
+								0.09019607843137255, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[360412] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[362405] = {
+								0, -- [1]
+								1, -- [2]
+								0.05882352941176471, -- [3]
+							},
+							[361815] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+							[365295] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+							[362849] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Kryxis the Voracious"] = {
+							[319685] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Halondrus the Reclaimer"] = {
+							[360115] = {
+								1, -- [1]
+								1, -- [2]
+								0, -- [3]
+							},
+						},
+					},
+					["barEmphasized"] = {
+						["BigWigs_Bosses_Kryxis the Voracious"] = {
+							[319685] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Lords of Dread"] = {
+							[360146] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+							[360319] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_The Jailer"] = {
+							[360562] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+							[363893] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Artificer Xy'mox v2"] = {
+							[362849] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Vigilant Guardian"] = {
+							[360412] = {
+								nil, -- [1]
+								0.8823529411764706, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Anduin Wrynn"] = {
+							[362405] = {
+								0, -- [1]
+								1, -- [2]
+							},
+							[361815] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+							[365295] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+							[361966] = {
+								0.9333333333333333, -- [1]
+								1, -- [2]
+								0.04705882352941176, -- [3]
+							},
+						},
+						["BigWigs_Bosses_Halls of Atonement Trash"] = {
+							[326409] = {
+								nil, -- [1]
+								1, -- [2]
+							},
+						},
+						["BigWigs_Bosses_Halondrus the Reclaimer"] = {
+							[360115] = {
+								nil, -- [1]
+								0.996078431372549, -- [2]
+							},
+						},
+					},
+				},
+			},
+		},
+		["BigWigs_Plugins_AutoReply"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["disabled"] = false,
+					["mode"] = 4,
+				},
+			},
+		},
+		["BigWigs_Plugins_InfoBox"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["posx"] = 1209.744217679428,
+					["posy"] = 543.1778594020798,
+				},
+			},
+		},
+		["BigWigs_Bosses_Skolex, the Insatiable Ravener"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[360451] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Halondrus the Reclaimer"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[360115] = 3129591,
+				},
+				["Healer Raid"] = {
+					[360115] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Plugins_Super Emphasize"] = {
+		},
+		["BigWigs_Plugins_Bars"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["barStyle"] = "ElvUI",
+					["BigWigsEmphasizeAnchor_height"] = 24.9999885559082,
+					["BigWigsAnchor_width"] = 225.0794677734375,
+					["BigWigsAnchor_y"] = 156.977923962424,
+					["fontName"] = "Expressway",
+					["BigWigsAnchor_height"] = 29.14285850524902,
+					["emphasizeGrowup"] = true,
+					["BigWigsAnchor_x"] = 329.0221043821384,
+					["spacing"] = 4,
+					["BigWigsEmphasizeAnchor_x"] = 343.7777956591708,
+					["BigWigsEmphasizeAnchor_width"] = 295.0000915527344,
+					["BigWigsEmphasizeAnchor_y"] = 232.1000692895468,
+					["emphasizeTime"] = 20,
+				},
+				["Default"] = {
+					["BigWigsAnchor_width"] = 220.0000610351563,
+					["BigWigsEmphasizeAnchor_height"] = 21.99996376037598,
+					["BigWigsAnchor_height"] = 15.99998664855957,
+				},
+			},
+		},
+		["BigWigs_Bosses_Echelon"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[319941] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_Dausegne, the Fallen Oracle"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[361018] = 3064311,
+				},
+				["Healer Raid"] = {
+					[361018] = 3064311,
+					[363200] = 3129591,
+					[365418] = 3129591,
+				},
+			},
+		},
+		["BigWigs_Bosses_The Necrotic Wake Trash"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[338606] = 3064311,
+					[323496] = 3064311,
+				},
+			},
+		},
+		["BigWigs_Plugins_Proximity"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					["posx"] = 1218.177884527991,
+					["posy"] = 439.0222276309178,
+					["width"] = 139.9999847412109,
+				},
+				["Default"] = {
+					["width"] = 139.9999847412109,
+				},
+			},
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["BigWigs_Plugins_Pull"] = {
+		},
+		["BigWigs_Bosses_Kul'tharok"] = {
+			["profiles"] = {
+				["Healer Universal"] = {
+					[319626] = 3129591,
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Trevata - Kazzak"] = "Healer Universal",
+		["Mesoto - Kazzak"] = "Healer Universal",
+	},
+	["profiles"] = {
+		["Healer Universal"] = {
+		},
+		["Default"] = {
+		},
+	},
+}
+BigWigsIconDB = {
+	["minimapPos"] = 205.1604724888275,
+	["hide"] = true,
+}
+BigWigsStatsDB = {
+	[2481] = {
+		[2461] = {
+			["normal"] = {
+				["kills"] = 6,
+				["wipes"] = 5,
+				["best"] = 227.0770000000484,
+			},
+			["heroic"] = {
+				["wipes"] = 3,
+			},
+		},
+		[2469] = {
+			["normal"] = {
+				["kills"] = 6,
+				["wipes"] = 26,
+				["best"] = 490.6799999999348,
+			},
+		},
+		[2470] = {
+			["normal"] = {
+				["best"] = 273.2839999999851,
+				["kills"] = 5,
+			},
+			["heroic"] = {
+				["kills"] = 3,
+				["wipes"] = 11,
+				["best"] = 340.783000000054,
+			},
+			["LFR"] = {
+				["best"] = 252.839999999851,
+				["kills"] = 2,
+			},
+		},
+		[2463] = {
+			["normal"] = {
+				["kills"] = 6,
+				["wipes"] = 7,
+				["best"] = 268.4320000000298,
+			},
+			["heroic"] = {
+				["kills"] = 1,
+				["wipes"] = 13,
+				["best"] = 451.5860000001267,
+			},
+			["LFR"] = {
+				["best"] = 307.466999999946,
+				["kills"] = 2,
+			},
+		},
+		[2464] = {
+			["normal"] = {
+				["kills"] = 3,
+				["wipes"] = 10,
+				["best"] = 501.1499999999069,
+			},
+		},
+		[2457] = {
+			["normal"] = {
+				["kills"] = 4,
+				["wipes"] = 11,
+				["best"] = 381.6720000002533,
+			},
+		},
+		[2465] = {
+			["normal"] = {
+				["kills"] = 5,
+				["best"] = 215.8249999999534,
+				["wipes"] = 2,
+			},
+			["heroic"] = {
+				["kills"] = 4,
+				["wipes"] = 21,
+				["best"] = 262.7939999999944,
+			},
+			["LFR"] = {
+				["best"] = 226.7330000000075,
+				["kills"] = 2,
+			},
+		},
+		[2458] = {
+			["heroic"] = {
+				["kills"] = 5,
+				["wipes"] = 7,
+				["best"] = 428.5360000000801,
+			},
+			["normal"] = {
+				["best"] = 363.2420000000857,
+				["kills"] = 7,
+			},
+			["LFR"] = {
+				["best"] = 377.4939999999479,
+				["kills"] = 2,
+			},
+		},
+		[2459] = {
+			["normal"] = {
+				["kills"] = 7,
+				["wipes"] = 3,
+				["best"] = 176.24599999981,
+			},
+			["heroic"] = {
+				["best"] = 230.5930000001099,
+				["kills"] = 1,
+			},
+		},
+		[2467] = {
+			["normal"] = {
+				["kills"] = 4,
+				["wipes"] = 3,
+				["best"] = 279.3249999997206,
+			},
+		},
+		[2460] = {
+			["normal"] = {
+				["kills"] = 7,
+				["wipes"] = 4,
+				["best"] = 231.1810000000987,
+			},
+			["heroic"] = {
+				["kills"] = 1,
+				["wipes"] = 3,
+				["best"] = 407.4029999999329,
+			},
+		},
+	},
+}
---
+++ msbt/MikScrollingBattleText.lua
@@ -0,0 +1,220 @@
+
+MSBTProfiles_SavedVars = {
+	["profiles"] = {
+		["DPS"] = {
+			["critFontName"] = "Expressway",
+			["events"] = {
+				["SELF_HOT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HEAL_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HOT_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HOT_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HOT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HEAL_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HEAL"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HEAL"] = {
+					["message"] = "(%s) +%a",
+				},
+			},
+			["soundsDisabled"] = true,
+			["enableBlizzardDamage"] = false,
+			["scrollAreas"] = {
+				["Incoming"] = {
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["offsetX"] = -300,
+					["animationStyle"] = "Straight",
+				},
+				["Static"] = {
+					["disabled"] = true,
+					["offsetY"] = -230,
+				},
+				["Outgoing"] = {
+					["stickyDirection"] = "Down",
+					["offsetX"] = 255,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["animationStyle"] = "Straight",
+				},
+				["Notification"] = {
+					["offsetY"] = 298,
+					["offsetX"] = -164,
+					["disabled"] = true,
+				},
+			},
+			["normalFontName"] = "Expressway",
+			["enableBlizzardHealing"] = false,
+			["creationVersion"] = "5.7.153",
+		},
+		["|cffffa500Benik|r|cffffffffUI|r Mesoto - Kazzak"] = {
+			["soundsDisabled"] = true,
+			["critFontName"] = "Expressway",
+			["enableBlizzardDamage"] = false,
+			["scrollAreas"] = {
+				["Notification"] = {
+					["offsetX"] = -164,
+					["offsetY"] = 298,
+				},
+				["Incoming"] = {
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["offsetX"] = -300,
+					["animationStyle"] = "Straight",
+				},
+				["Outgoing"] = {
+					["stickyDirection"] = "Down",
+					["offsetX"] = 255,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["animationStyle"] = "Straight",
+				},
+				["Static"] = {
+					["disabled"] = true,
+					["offsetY"] = -230,
+				},
+			},
+			["normalFontName"] = "Expressway",
+			["enableBlizzardHealing"] = false,
+			["creationVersion"] = "5.7.153",
+		},
+		["Default"] = {
+			["events"] = {
+				["SELF_HOT"] = {
+					["message"] = "%s +%a",
+				},
+				["INCOMING_HEAL_CRIT"] = {
+					["message"] = "%s +%a",
+				},
+				["INCOMING_HOT_CRIT"] = {
+					["message"] = "%s +%a",
+				},
+				["SELF_HOT_CRIT"] = {
+					["message"] = "%s +%a",
+				},
+				["INCOMING_HOT"] = {
+					["message"] = "%s +%a",
+				},
+				["SELF_HEAL_CRIT"] = {
+					["message"] = "%s +%a",
+				},
+				["INCOMING_HEAL"] = {
+					["message"] = "%s +%a",
+				},
+				["SELF_HEAL"] = {
+					["message"] = "%s +%a",
+				},
+			},
+			["enableBlizzardDamage"] = false,
+			["scrollAreas"] = {
+				["Static"] = {
+					["normalFontName"] = "Univers",
+					["disabled"] = true,
+					["critFontName"] = "Univers",
+				},
+				["Incoming"] = {
+					["direction"] = "Up",
+					["offsetX"] = -180,
+					["critFontName"] = "Univers",
+					["behavior"] = "MSBT_NORMAL",
+					["normalFontName"] = "Univers",
+					["animationStyle"] = "Straight",
+					["offsetY"] = -159,
+				},
+				["Notification"] = {
+					["critFontName"] = "Univers",
+					["normalFontName"] = "Univers",
+					["offsetX"] = -172,
+					["offsetY"] = 275,
+				},
+				["Outgoing"] = {
+					["critFontName"] = "Univers",
+					["behavior"] = "MSBT_NORMAL",
+					["normalFontName"] = "Univers",
+					["animationStyle"] = "Straight",
+					["offsetX"] = 150,
+				},
+			},
+			["soundsDisabled"] = true,
+			["enableBlizzardHealing"] = false,
+			["creationVersion"] = "5.7.153",
+		},
+		["Healer M+"] = {
+			["critFontName"] = "Expressway",
+			["events"] = {
+				["SELF_HOT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HEAL_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HOT_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HOT_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HOT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HEAL_CRIT"] = {
+					["message"] = "(%s) +%a",
+				},
+				["INCOMING_HEAL"] = {
+					["message"] = "(%s) +%a",
+				},
+				["SELF_HEAL"] = {
+					["message"] = "(%s) +%a",
+				},
+			},
+			["soundsDisabled"] = true,
+			["enableBlizzardDamage"] = false,
+			["scrollAreas"] = {
+				["Incoming"] = {
+					["offsetX"] = -300,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["animationStyle"] = "Straight",
+					["disabled"] = true,
+				},
+				["Static"] = {
+					["disabled"] = true,
+					["offsetY"] = -230,
+				},
+				["Outgoing"] = {
+					["stickyDirection"] = "Down",
+					["offsetX"] = 255,
+					["behavior"] = "MSBT_NORMAL",
+					["offsetY"] = -145,
+					["animationStyle"] = "Straight",
+				},
+				["Notification"] = {
+					["offsetY"] = 298,
+					["disabled"] = true,
+					["offsetX"] = -164,
+				},
+			},
+			["normalFontName"] = "Expressway",
+			["enableBlizzardHealing"] = false,
+			["creationVersion"] = "5.7.153",
+		},
+	},
+}
+MSBT_SavedMedia = {
+	["fonts"] = {
+	},
+	["sounds"] = {
+	},
+}
